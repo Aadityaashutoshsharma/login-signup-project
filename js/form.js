1995e80f1ff5e69c7412eb904159320c2fa8a85e
Aadityaashutoshsharma <aadityaashutoshsharma@gmail.com>
2023-02-19 15:06:31 +0530

    Using Ajax to make the website to be hosted

--- js/form.js
+++ js/form.js
@@ -4,15 +4,16 @@ function signUpSubmit(firstName, lastName, email, password, mobile) {
     var parems = {
         firstName : firstName.value,
         lastName: lastName.value,
-        email: email.value,
+        email_address: email.value,
         password: password.value,
         mobile: mobile.value
     }
     xhr.open('POST', 'http://localhost:8080/api/v1/signup');
+    xhr.setRequestHeader('Content-Type', 'aaplication/json;charset=UTF-8');
     xhr.send(JSON.stringify(parems));
     xhr.onreadystatechange = sendRequest;
 }
 
 function sendRequest() {
-    
+    console.log(xhr.readyState);
 }
